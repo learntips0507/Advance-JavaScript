b4ab10a574f2ceb122adee85d4c1f798389e9cd6
learntips0507 <learntips0507@gmail.com>
2020-03-10 03:40:32 +0600

    true and false

---
+++ true-false.js
@@ -0,0 +1,24 @@
+//false
+//0
+//undefined
+//false
+//null
+//NaN(not a number)
+//true
+//"0"
+//1,2,3 any number
+//"any name"
+//true
+let number = null;
+if(number  || number == 0){
+    console.log("Condition is true");
+}else{
+    console.log("condition is false");
+}
+
+let name = '0';
+if(name  || name == 0){
+    console.log("Condition is true");
+}else{
+    console.log("condition is false");
+}
