0d853961758002184bc46396160caec3707af7f0
learntips0507 <learntips0507@gmail.com>
2020-03-11 00:20:33 +0600

    Null VS Undefined cleared concept

--- nullvsundefined.js
+++ nullvsundefined.js
@@ -1,5 +1,34 @@
-let name;
-console.log(name);
+//it will show Undefined
+let a;
+console.log(a);
 
-const student = {name: "MD.SHAHJAHAN", phone:01929880507};
-console.log
+const student = {name:"SHAHJAHAN", phone:01929880507};
+console.log(student.user);
+function student(a,b){
+    const total = a+b;
+    return
+}
+const result = student(5,6);
+console.log(result);
+
+function student(a,b){
+    const total = a+b;
+    return
+}
+const result = student(5);
+console.log(result);
+
+function student(a,b){
+    console.log(b);
+}
+const result = student(6);
+console.log(result);
+
+let a = null;
+console.log(a);
+
+let a = null;
+let b;
+console.log(typeof(a));
+console.log(typeof(b));
+
